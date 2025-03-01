f691cbae5e43a8b829a2e96d54aa730f0368b934
Lada <ladasergeevna@mail.ru>
2025-03-01 22:55:17 +0900

    Tests have been added

--- skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
+++ skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -33,7 +33,8 @@ public class StorageService {
         Article sweetArticle = new Article("Инфо о конфетах в упаковке", "Конфеты из молочного шоколада в подарочной коробке", UUID.randomUUID());
         Article chocolateArticle = new Article("Инфо о конфетах", "Конфеты из молочного шоколада с начинкой из шоколада", UUID.randomUUID());
         Article lollipopArticle = new Article("Инфо о леденцах", "Конфеты леденцовые", UUID.randomUUID());
-        this.products.put(rollers.getId(), rollers);
+       // this.products.put(rollers.getId(), rollers);
+        this.products.put(UUID.fromString("00000000-0000-0000-0000-000000000000"), rollers);
         this.products.put(dress.getId(), dress);
         this.products.put(cream.getId(), cream);
         this.products.put(sweets.getId(), sweets);
